8941394d768a4eaee31576924d4632f0e3745f86
mathanaravi <65063173+mathanaravi@users.noreply.github.com>
2024-12-03 14:26:35 +0800

    Create Print.py

---
+++ Expressions/Easy/Print.py
@@ -0,0 +1,25 @@
+# Print my name
+Name = "Mathana"
+print(Name)
+
+# Assign value to a constant and print it
+PI = 3.14
+print(PI)
+
+
+# Display data types of integer, float and string
+a = 5
+print(type(a))
+b = 7.5
+print(type(b))
+c="3"
+print(type(c))
+
+
+# Print a statement
+Name = "Mathana"
+print("Hello, " + Name + "!" + " Welcome to Python programming.")
+
+# Print your Age
+Age = input("How old are you?: ")
+print("You are " + Age + " years old.")
